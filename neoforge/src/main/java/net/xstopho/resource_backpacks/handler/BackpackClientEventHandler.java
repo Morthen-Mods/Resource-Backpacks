b91213e06e8ce6f43c3b451167ae3c0c8f79be52
Stein-N <nico-stein-98@outlook.de>
2024-11-08 17:13:49 +0100

    - finalize update to 1.21.3

--- neoforge/src/main/java/net/xstopho/resource_backpacks/handler/BackpackClientEventHandler.java
+++ neoforge/src/main/java/net/xstopho/resource_backpacks/handler/BackpackClientEventHandler.java
@@ -1,11 +1,16 @@
 package net.xstopho.resource_backpacks.handler;
 
+import com.misterpemodder.shulkerboxtooltip.api.neoforge.ShulkerBoxTooltipPlugin;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.xstopho.resource_backpacks.BackpackConstants;
+import net.xstopho.resource_backpacks.compat.shulkerboxtooltip.BackpackTooltipPlugin;
 import net.xstopho.resource_backpacks.registries.KeyMappingRegistry;
 import net.xstopho.resource_backpacks.registries.MenuTypeRegistry;
 import net.xstopho.resource_backpacks.rendering.container.BackpackContainerScreen;
@@ -13,6 +18,14 @@ import net.xstopho.resource_backpacks.rendering.container.BackpackContainerScree
 
 @EventBusSubscriber(modid = BackpackConstants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class BackpackClientEventHandler {
+
+    @SubscribeEvent
+    public static void registerCommonStuff(FMLClientSetupEvent event) {
+        if (ModList.get().isLoaded("shulkerboxtooltip")) {
+            ModLoadingContext.get().registerExtensionPoint(ShulkerBoxTooltipPlugin.class,
+                    () -> new ShulkerBoxTooltipPlugin(BackpackTooltipPlugin::new));
+        }
+    }
 
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
