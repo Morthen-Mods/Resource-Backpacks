601e4403c6e105c73cb8a3aaa11fb2cb624c3872
Stein-N <nico-stein-98@outlook.de>
2024-11-11 19:16:59 +0100

    changing registry for BlockEntityTypes

--- common/src/main/java/net/xstopho/resource_backpacks/registries/BlockEntityRegistry.java
+++ common/src/main/java/net/xstopho/resource_backpacks/registries/BlockEntityRegistry.java
@@ -1,7 +1,6 @@
 package net.xstopho.resource_backpacks.registries;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.xstopho.resource_backpacks.BackpackConstants;
 import net.xstopho.resource_backpacks.custom.entities.BackpackBlockEntity;
@@ -14,11 +13,8 @@ public class BlockEntityRegistry {
 
     private static final RegistryProvider<BlockEntityType<?>> BLOCK_ENTITIES = RegistryProvider.get(BackpackConstants.MOD_ID, BuiltInRegistries.BLOCK_ENTITY_TYPE);
 
-    public static final RegistryObject<BlockEntityType<?>> BACKPACK_ENTITY = register("backpack_entity", BackpackBlockEntity::new, BlockRegistry.TEST_BACKPACK.get());
-
-    private static RegistryObject<BlockEntityType<?>> register(String id, BlockEntityType.BlockEntitySupplier<?> blockEntitySupplier, Block... blocks) {
-        return BLOCK_ENTITIES.register(id, () -> new BlockEntityType<>(blockEntitySupplier, Set.of(blocks)));
-    }
+    public static final RegistryObject<BlockEntityType<BackpackBlockEntity>> BACKPACK_ENTITY = BLOCK_ENTITIES.register("backpack_entity",
+            () -> new BlockEntityType<>(BackpackBlockEntity::new, Set.of(BlockRegistry.TEST_BACKPACK.get())));
 
     public static void init() {}
 }
