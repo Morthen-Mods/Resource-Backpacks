24df512a03bd41e0b0416b73b73f4ca27d65edff
Stein-N <nico-stein-98@outlook.de>
2024-11-12 13:43:53 +0100

    add CreativeTab

--- common/src/main/java/net/xstopho/resource_backpacks/registries/CreativeTabRegistry.java
+++ common/src/main/java/net/xstopho/resource_backpacks/registries/CreativeTabRegistry.java
@@ -1,13 +1,25 @@
 package net.xstopho.resource_backpacks.registries;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.xstopho.resource_backpacks.BackpackConstants;
+import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
 
 public class CreativeTabRegistry {
 
     private static final RegistryProvider<CreativeModeTab> CREATIVE_TAB = RegistryProvider.get(BackpackConstants.MOD_ID, BuiltInRegistries.CREATIVE_MODE_TAB);
+
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_TAB.register("backpack_tab",
+            () -> CreativeModeTab.builder(null, -1).title(Component.translatable("tab.resource_bacpacks.item_group"))
+                    .icon(() -> new ItemStack(BlockRegistry.TEST_BACKPACK.get()))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(BlockRegistry.TEST_BACKPACK.get());
+
+                    }).build());
 
     public static void init() {}
 }
