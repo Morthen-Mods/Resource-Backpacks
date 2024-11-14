ad34757861a3de6a5a9038e247726594b503e66b
Stein-N <nico-stein-98@outlook.de>
2024-11-14 17:31:44 +0100

    adding Config with ranged Values and better comments as before

--- common/src/main/java/net/xstopho/resource_backpacks/util/BackpackLevel.java
+++ common/src/main/java/net/xstopho/resource_backpacks/util/BackpackLevel.java
@@ -1,37 +1,45 @@
 package net.xstopho.resource_backpacks.util;
 
+import java.util.function.Supplier;
+
+import static net.xstopho.resource_backpacks.BackpackConfig.*;
+
 public enum BackpackLevel {
 
-    DEFAULT(5, 14),
-    LEATHER(3, 9),
-    COPPER(3, 10),
-    GOLD(4, 11),
-    IRON(5, 12),
-    DIAMOND(6, 12),
-    NETHERITE(7, 13),
-    END(3, 9);
+    DEFAULT(() -> 5, () -> 14),
+    LEATHER(LEATHER_ROWS, LEATHER_COLUMNS),
+    COPPER(COPPER_ROWS, COPPER_COLUMNS),
+    GOLD(GOLD_ROWS, GOLD_COLUMNS),
+    IRON(IRON_ROWS, IRON_COLUMNS),
+    DIAMOND(DIAMOND_ROWS, DIAMOND_COLUMNS),
+    NETHERITE(NETHERITE_ROWS, NETHERITE_COLUMNS),
+    END(() -> 3, () -> 9);
 
-    final int rows, columns;
+    final Supplier<Integer> rows, columns;
 
-    BackpackLevel(int rows, int columns) {
+    BackpackLevel(Supplier<Integer> rows, Supplier<Integer> columns) {
         this.rows = rows;
         this.columns = columns;
     }
 
     public int getRows() {
-        return rows;
+
+        return rows.get();
     }
 
     public int getColumns() {
-        return columns;
+
+        return columns.get();
     }
 
     public int getSize() {
-        return this.columns * this.rows;
+
+        return this.getRows() * this.getColumns();
     }
 
     @Override
     public String toString() {
+
         return this.name().toLowerCase();
     }
 }
