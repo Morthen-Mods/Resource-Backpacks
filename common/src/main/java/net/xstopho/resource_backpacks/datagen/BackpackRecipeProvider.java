e0686f16953de4a62f2bd650f42734acfd747730
Stein-N <nico-stein-98@outlook.de>
2024-11-14 19:11:31 +0100

    - change Netherite Backpack recipe to make it more expensive - cleanup to get use of BackpackItemTags

--- common/src/main/java/net/xstopho/resource_backpacks/datagen/BackpackRecipeProvider.java
+++ common/src/main/java/net/xstopho/resource_backpacks/datagen/BackpackRecipeProvider.java
@@ -29,12 +29,20 @@ public class BackpackRecipeProvider extends RecipeProvider {
     @Override
     public void buildRecipes() {
         backpackRecipe(Items.CHEST, BackpackItemTags.BACKPACK_LEATHER_INGREDIENT, BlockRegistry.BACKPACK_LEATHER.get());
-        backpackRecipe(BlockRegistry.BACKPACK_LEATHER.get(), TagUtil.createItemTag("copper_ingots"), BlockRegistry.BACKPACK_COPPER.get());
-        backpackRecipe(BlockRegistry.BACKPACK_COPPER.get(), TagUtil.createItemTag("gold_ingots"), BlockRegistry.BACKPACK_GOLD.get());
-        backpackRecipe(BlockRegistry.BACKPACK_GOLD.get(), TagUtil.createItemTag("iron_ingots"), BlockRegistry.BACKPACK_IRON.get());
-        backpackRecipe(BlockRegistry.BACKPACK_IRON.get(), TagUtil.createItemTag("diamonds"), BlockRegistry.BACKPACK_DIAMOND.get());
+        backpackRecipe(BlockRegistry.BACKPACK_LEATHER.get(), BackpackItemTags.COPPER_INGOTS, BlockRegistry.BACKPACK_COPPER.get());
+        backpackRecipe(BlockRegistry.BACKPACK_COPPER.get(), BackpackItemTags.GOLD_INGOTS, BlockRegistry.BACKPACK_GOLD.get());
+        backpackRecipe(BlockRegistry.BACKPACK_GOLD.get(), BackpackItemTags.IRON_INGOTS, BlockRegistry.BACKPACK_IRON.get());
+        backpackRecipe(BlockRegistry.BACKPACK_IRON.get(), BackpackItemTags.DIAMONDS, BlockRegistry.BACKPACK_DIAMOND.get());
 
-        this.shaped(RecipeCategory.MISC, BlockRegistry.BACKPACK_END.get(), 1)
+        this.shaped(RecipeCategory.MISC, BlockRegistry.BACKPACK_NETHERITE.get())
+                .pattern("NDN").pattern("DBD").pattern("NDN")
+                .define('N', BackpackItemTags.NETHERITE_INGOTS)
+                .define('D', BackpackItemTags.DIAMONDS)
+                .define('B', BlockRegistry.BACKPACK_DIAMOND.get())
+                .unlockedBy("has_diamond_backpack", has(BlockRegistry.BACKPACK_DIAMOND.get()))
+                .save(this.output, key(BlockRegistry.BACKPACK_NETHERITE.get()));
+
+        this.shaped(RecipeCategory.MISC, BlockRegistry.BACKPACK_END.get())
                 .pattern("DCD").pattern("EBE").pattern("DCD")
                 .define('D', Items.DIAMOND)
                 .define('C', Items.ENDER_CHEST)
@@ -42,21 +50,6 @@ public class BackpackRecipeProvider extends RecipeProvider {
                 .define('B', BlockRegistry.BACKPACK_IRON.get())
                 .unlockedBy(getHasName(BlockRegistry.BACKPACK_IRON.get()), has(BlockRegistry.BACKPACK_IRON.get()))
                 .save(output, key(BlockRegistry.BACKPACK_END.get()));
-
-        SmithingTransformRecipeBuilder.smithing(Ingredient.of(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE),
-                Ingredient.of(BlockRegistry.BACKPACK_IRON.get()), Ingredient.of(Items.NETHERITE_INGOT),
-                RecipeCategory.MISC, BlockRegistry.BACKPACK_NETHERITE.get().asItem())
-                .unlocks(getHasName(BlockRegistry.BACKPACK_DIAMOND.get()), has(BlockRegistry.BACKPACK_DIAMOND.get()))
-                .save(this.output, key(BlockRegistry.BACKPACK_NETHERITE.get()));
-    }
-
-    private void backpackRecipe(TagKey<Item> input, TagKey<Item> upgradeMaterial, ItemLike output) {
-        this.shaped(RecipeCategory.MISC, output)
-                .pattern("UUU").pattern("UBU").pattern("UUU")
-                .define('U', upgradeMaterial)
-                .define('B', input)
-                .unlockedBy("has_chest", has(input))
-                .save(this.output, key(output));
     }
 
     private void backpackRecipe(ItemLike input, TagKey<Item> upgradeMaterial, ItemLike output) {
