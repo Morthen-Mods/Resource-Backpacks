424ceaac6cf0e6042772ae1157fd379c09cf9a10
Stein-N <nico-stein-98@outlook.de>
2024-11-15 17:09:43 +0100

    all: adding ShulkerBoxTooltip compatibility

--- neoforge/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
+++ neoforge/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
@@ -1,12 +1,25 @@
 package net.xstopho.resource_backpacks;
 
+import com.misterpemodder.shulkerboxtooltip.api.neoforge.ShulkerBoxTooltipPlugin;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
+import net.xstopho.resource_backpacks.compat.BackpackTooltipPlugin;
 
 @Mod(BackpackConstants.MOD_ID)
 public class ResourceBackpacks {
 
     public ResourceBackpacks(IEventBus eventBus) {
         BackpackConstants.commonInit();
+
+        if (FMLLoader.getDist().equals(Dist.CLIENT)) {
+            if (ModList.get().isLoaded("shulkerboxtooltip")) {
+                ModLoadingContext.get().registerExtensionPoint(ShulkerBoxTooltipPlugin.class,
+                        () -> new ShulkerBoxTooltipPlugin(BackpackTooltipPlugin::new));
+            }
+        }
     }
 }
