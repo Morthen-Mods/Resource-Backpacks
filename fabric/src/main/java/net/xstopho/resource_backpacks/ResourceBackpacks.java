c28c685b43e9c5ec9e63c9b4fb6fe3bcf75d42a5
Stein-N <nico-stein-98@outlook.de>
2024-11-15 12:51:01 +0100

    all: moving ConfigRegistry to BackpackConstants.commonInit

--- fabric/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
+++ fabric/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
@@ -2,13 +2,10 @@ package net.xstopho.resource_backpacks;
 
 import net.fabricmc.api.ModInitializer;
 import net.xstopho.resource_backpacks.network.BackpackNetwork;
-import net.xstopho.resourceconfigapi.api.ConfigRegistry;
 
 public class ResourceBackpacks implements ModInitializer {
     @Override
     public void onInitialize() {
-        ConfigRegistry.register(BackpackConstants.MOD_ID, BackpackConfig.BUILDER, false);
-
         BackpackNetwork.initPayloads();
 
         BackpackConstants.commonInit();
