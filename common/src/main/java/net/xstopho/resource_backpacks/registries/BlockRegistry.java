333e92bd2534abf8a9cdefc70c61d5b9c048803c
Stein-N <nico-stein-98@outlook.de>
2024-11-19 18:00:54 +0100

    common: removing test Backpack from Registry

--- common/src/main/java/net/xstopho/resource_backpacks/registries/BlockRegistry.java
+++ common/src/main/java/net/xstopho/resource_backpacks/registries/BlockRegistry.java
@@ -22,7 +22,6 @@ public class BlockRegistry {
     private static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(Registries.BLOCK, BackpackConstants.MOD_ID);
     private static final RegistryProvider<Item> ITEMS = RegistryProvider.get(Registries.ITEM, BackpackConstants.MOD_ID);
 
-    public static final RegistryObject<Block> BACKPACK_TEST = registerBlock("backpack_test", BackpackLevel.DEFAULT);
     public static final RegistryObject<Block> BACKPACK_LEATHER = registerBlock("backpack_leather", BackpackLevel.LEATHER);
     public static final RegistryObject<Block> BACKPACK_COPPER = registerBlock("backpack_copper", BackpackLevel.COPPER);
     public static final RegistryObject<Block> BACKPACK_GOLD = registerBlock("backpack_gold", BackpackLevel.GOLD);
