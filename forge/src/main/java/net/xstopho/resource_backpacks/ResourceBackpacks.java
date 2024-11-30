936c5dcf3d89adafce91d9d5457b8ca46292ef38
Stein-N <nico-stein-98@outlook.de>
2024-11-30 12:06:14 +0100

    project: remove any compatibility repo or mod implementation and bump versions

--- forge/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
+++ forge/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
@@ -1,14 +1,9 @@
 package net.xstopho.resource_backpacks;
 
-import com.misterpemodder.shulkerboxtooltip.api.forge.ShulkerBoxTooltipPlugin;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.network.SimpleChannel;
-import net.xstopho.resource_backpacks.compat.BackpackTooltipPlugin;
 import net.xstopho.resource_backpacks.network.BackpackNetwork;
 
 @Mod(BackpackConstants.MOD_ID)
@@ -20,13 +15,6 @@ public class ResourceBackpacks {
         context.getModEventBus().addListener(this::initCommon);
 
         BackpackConstants.commonInit();
-
-        if (FMLLoader.getDist().equals(Dist.CLIENT)) {
-            if (ModList.get().isLoaded("shulkerboxtooltip")) {
-                context.registerExtensionPoint(ShulkerBoxTooltipPlugin.class,
-                        () -> new ShulkerBoxTooltipPlugin(BackpackTooltipPlugin::new));
-            }
-        }
     }
 
     private void initCommon(FMLCommonSetupEvent event) {
