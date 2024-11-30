47a87c738e4d026fa30013316bb1ba7b6b8acddf
Stein-N <nico-stein-98@outlook.de>
2024-11-30 12:24:07 +0100

    fabric: porting networking and services

--- fabric/src/main/java/net/xstopho/resource_backpacks/network/BackpackNetwork.java
+++ fabric/src/main/java/net/xstopho/resource_backpacks/network/BackpackNetwork.java
@@ -1,5 +1,6 @@
 package net.xstopho.resource_backpacks.network;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
@@ -8,5 +9,14 @@ public class BackpackNetwork {
     public static void initPayloads() {
         PayloadTypeRegistry.playC2S().register(OpenBackpackPayload.PACKET_TYPE, OpenBackpackPayload.PACKET_CODEC);
         ServerPlayNetworking.registerGlobalReceiver(OpenBackpackPayload.PACKET_TYPE, OpenBackpackPayload::apply);
+
+        PayloadTypeRegistry.playC2S().register(EnderChestRequestPayload.TYPE, EnderChestRequestPayload.CODEC);
+        ServerPlayNetworking.registerGlobalReceiver(EnderChestRequestPayload.TYPE, EnderChestRequestPayload::apply);
+
+        PayloadTypeRegistry.playS2C().register(EnderChestResponsePayload.TYPE, EnderChestResponsePayload.CODEC);
+    }
+
+    public static void initClientPayloads() {
+        ClientPlayNetworking.registerGlobalReceiver(EnderChestResponsePayload.TYPE, EnderChestResponsePayload::apply);
     }
 }
