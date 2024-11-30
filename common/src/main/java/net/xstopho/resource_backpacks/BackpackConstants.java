48232bc96eeaf06dd2faefbcb4eb2ce1ded5e4ff
Stein-N <nico-stein-98@outlook.de>
2024-11-30 12:13:29 +0100

    common: add needed helper methods

--- common/src/main/java/net/xstopho/resource_backpacks/BackpackConstants.java
+++ common/src/main/java/net/xstopho/resource_backpacks/BackpackConstants.java
@@ -1,14 +1,24 @@
 package net.xstopho.resource_backpacks;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.xstopho.resource_backpacks.registries.BlockEntityRegistry;
 import net.xstopho.resource_backpacks.registries.BlockRegistry;
 import net.xstopho.resource_backpacks.registries.CreativeTabRegistry;
 import net.xstopho.resource_backpacks.registries.MenuTypeRegistry;
 import net.xstopho.resource_backpacks.screen.BackpackMenuScreen;
+import net.xstopho.resource_backpacks.util.CommonNetworkHook;
+import net.xstopho.resource_backpacks.util.KeyMappingInterface;
+import net.xstopho.resourceconfigapi.ResourceConfigConstants;
 import net.xstopho.resourceconfigapi.api.ConfigRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ServiceLoader;
 
 public class BackpackConstants {
     public static final String MOD_ID = "resource_backpacks";
@@ -35,5 +45,32 @@ public class BackpackConstants {
         MenuScreens.register(MenuTypeRegistry.DIAMOND_MENU.get(), BackpackMenuScreen::new);
         MenuScreens.register(MenuTypeRegistry.NETHERITE_MENU.get(), BackpackMenuScreen::new);
         MenuScreens.register(MenuTypeRegistry.END_MENU.get(), BackpackMenuScreen::new);
+    }
+
+    public static ResourceLocation of(String id) {
+
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, id);
+    }
+
+    public static boolean hasKeyDown(KeyMapping keyMapping) {
+        int keyCode = ((KeyMappingInterface) keyMapping).getKey().getValue();
+        return InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), keyCode);
+    }
+
+    public static Component getKeyName(KeyMapping keyMapping) {
+        return ((KeyMappingInterface) keyMapping).getKey().getDisplayName();
+    }
+
+    public static void requestEnderChestContainer() {
+
+        load(CommonNetworkHook.class).sendEnderChestRequest();
+    }
+
+    public static <T> T load(Class<T> clazz) {
+        final T loadedService = ServiceLoader.load(clazz)
+                .findFirst()
+                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+        ResourceConfigConstants.LOG.debug("Loaded {} for service {}", loadedService, clazz);
+        return loadedService;
     }
 }
