67e717a9298412f916fd80bdc92378a2d3eae7d9
Stein-N <nico-stein-98@outlook.de>
2024-12-06 20:33:23 +0100

    common: remove ItemContainer Mixin and Accessor

--- common/src/main/java/net/xstopho/resource_backpacks/BackpackConstants.java
+++ common/src/main/java/net/xstopho/resource_backpacks/BackpackConstants.java
@@ -6,7 +6,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
 import net.xstopho.resource_backpacks.registries.BlockEntityRegistry;
 import net.xstopho.resource_backpacks.registries.BlockRegistry;
 import net.xstopho.resource_backpacks.registries.CreativeTabRegistry;
@@ -18,14 +17,12 @@ import net.xstopho.resourceconfigapi.api.ConfigRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ServiceLoader;
 
 public class BackpackConstants {
     public static final String MOD_ID = "resource_backpacks";
     public static final String MOD_NAME = "Resource Backpacks";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
-
-    public static final Map<UUID, List<ItemStack>> ENDER_CHESTS = new HashMap<>();
 
     public static void commonInit() {
         ConfigRegistry.register(MOD_ID, BackpackConfig.BUILDER, false);
--- common/src/main/java/net/xstopho/resource_backpacks/util/BackpackUtils.java
+++ common/src/main/java/net/xstopho/resource_backpacks/util/BackpackUtils.java
@@ -14,14 +14,6 @@ public class BackpackUtils {
     }
 
     /**
-     * Used to get the Items of the {@link net.minecraft.world.item.component.ItemContainerContents} since there
-     * isn't a native method to get the NonNullList as it is.
-     */
-    public interface ItemContainerAccess {
-        NonNullList<ItemStack> backpack$getItemsForPreview();
-    }
-
-    /**
      * Used to get the current set Key, because {@link net.minecraft.client.KeyMapping}
      * only allows to get the default key.
      */
