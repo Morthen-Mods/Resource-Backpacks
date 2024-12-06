8d379b7148ccb319e90c120a26b06db6c3f813f9
Stein-N <nico-stein-98@outlook.de>
2024-12-06 21:10:28 +0100

    fixing KeyMappingMixin

--- common/src/main/java/net/xstopho/resource_backpacks/mixin/client/KeyMappingMixin.java
+++ common/src/main/java/net/xstopho/resource_backpacks/mixin/client/KeyMappingMixin.java
@@ -2,10 +2,11 @@ package net.xstopho.resource_backpacks.mixin.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.xstopho.resource_backpacks.util.BackpackUtils;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(KeyMapping.class)
-public abstract class KeyMappingMixin implements KeyMappingInterface {
+public abstract class KeyMappingMixin implements BackpackUtils.KeyMappingAccess {
 
     @Override
     public InputConstants.Key getKey() {
