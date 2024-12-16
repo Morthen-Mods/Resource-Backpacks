700cf0b2a0905430330d7b09e2c27b9d981ba727
Stein-N <nico-stein-98@outlook.de>
2024-12-16 15:59:16 +0100

    fixing #28

---
+++ common/src/main/java/net/xstopho/resource_backpacks/client/BackpackModel.java
@@ -0,0 +1,48 @@
+package net.xstopho.resource_backpacks.client;
+
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.xstopho.resource_backpacks.BackpackConstants;
+import net.xstopho.resource_backpacks.backpack.BackpackItem;
+
+public class BackpackModel<T extends LivingEntityRenderState> extends EntityModel<T> {
+
+    public static final ModelLayerLocation BACKPACK_LAYER = new ModelLayerLocation(ResourceLocation.withDefaultNamespace("player"), "backpack");
+
+    public BackpackModel(ModelPart root) {
+        super(root);
+        root.getChild("backpack");
+    }
+
+    public static LayerDefinition createLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        partdefinition.addOrReplaceChild("backpack",
+                CubeListBuilder.create()
+                        .texOffs(0, 6).addBox("base", 0, 1, 0, 8, 9, 4)
+                        .texOffs(0, 0).addBox("bottom", 1, 10, 0, 6, 1, 4)
+                        .texOffs(21, 0).addBox("left_pouch", 8, 6, 0.5f, 1, 4, 3)
+                        .texOffs(21, 0).addBox("right_pouch", -1, 6, 0.5f, 1, 4, 3)
+                        .texOffs(25, 11).addBox("middle_pouch", 1, 3, 4, 6, 7, 1)
+                        .texOffs(30, 3).addBox("decorator", 2.5f, 6.5f, 4.5f, 3, 3, 1),
+                PartPose.offset(-4.0F, 0.0F, 2.0F));
+
+        return LayerDefinition.create(meshdefinition, 39, 19);
+    }
+
+    public static ResourceLocation getTexture(ItemStack stack) {
+        String type = "default";
+        if (stack.getItem() instanceof BackpackItem backpackItem) {
+            type = backpackItem.getBackpackLevel().name().toLowerCase();
+        }
+
+        return BackpackConstants.of("textures/entity/backpack_" + type + ".png");
+    }
+}
--- neoforge/src/main/java/net/xstopho/resource_backpacks/handler/ModClientHandler.java
+++ neoforge/src/main/java/net/xstopho/resource_backpacks/handler/ModClientHandler.java
@@ -1,11 +1,22 @@
 package net.xstopho.resource_backpacks.handler;
 
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.state.ArmorStandRenderState;
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraft.client.resources.PlayerSkin;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.xstopho.resource_backpacks.BackpackConstants;
+import net.xstopho.resource_backpacks.client.BackpackModel;
+import net.xstopho.resource_backpacks.client.BackpackRenderLayer;
 import net.xstopho.resource_backpacks.registries.KeyMappingRegistry;
 
 @EventBusSubscriber(modid = BackpackConstants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -21,5 +32,28 @@ public class ModClientHandler {
         event.register(KeyMappingRegistry.OPEN_BACKPACK);
         event.register(KeyMappingRegistry.SHOW_COMPACT_PREVIEW);
         event.register(KeyMappingRegistry.SHOW_INVENTORY_PREVIEW);
+    }
+
+    @SubscribeEvent
+    public static void registerRenderLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(BackpackModel.BACKPACK_LAYER, BackpackModel::createLayer);
+    }
+
+    @SubscribeEvent
+    public static void addRenderLayer(EntityRenderersEvent.AddLayers event) {
+        LivingEntityRenderer<Player, PlayerRenderState, HumanoidModel<PlayerRenderState>> playerRenderer = event.getSkin(PlayerSkin.Model.WIDE);
+        if (playerRenderer != null){
+            playerRenderer.addLayer(new BackpackRenderLayer<>(playerRenderer, event.getEntityModels()));
+        }
+
+        LivingEntityRenderer<Player, PlayerRenderState, HumanoidModel<PlayerRenderState>> playerSlimRenderer = event.getSkin(PlayerSkin.Model.SLIM);
+        if (playerSlimRenderer != null) {
+            playerSlimRenderer.addLayer(new BackpackRenderLayer<>(playerSlimRenderer, event.getEntityModels()));
+        }
+
+        LivingEntityRenderer<ArmorStand, ArmorStandRenderState, HumanoidModel<ArmorStandRenderState>> armorStandRenderer = event.getRenderer(EntityType.ARMOR_STAND);
+        if (armorStandRenderer != null) {
+            armorStandRenderer.addLayer(new BackpackRenderLayer<>(armorStandRenderer, event.getEntityModels()));
+        }
     }
 }
