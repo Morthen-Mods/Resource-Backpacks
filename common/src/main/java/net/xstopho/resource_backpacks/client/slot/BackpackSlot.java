50b7c197a9a117f4fe218598d3addbcd995de7f6
Stein-N <nico-stein-98@outlook.de>
2025-01-14 19:12:26 +0100

    add render helper methods to BackpackSlot

--- common/src/main/java/net/xstopho/resource_backpacks/client/slot/BackpackSlot.java
+++ common/src/main/java/net/xstopho/resource_backpacks/client/slot/BackpackSlot.java
@@ -1,11 +1,17 @@
 package net.xstopho.resource_backpacks.client.slot;
 
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.xstopho.resource_backpacks.BackpackConstants;
 import net.xstopho.resource_backpacks.backpack.BackpackItem;
 
 public class BackpackSlot extends Slot {
+    private static final ResourceLocation BACKPACK_SPRITE = BackpackConstants.of("textures/gui/sprites/container/backpack.png");
+    private static final ResourceLocation SLOT = ResourceLocation.withDefaultNamespace("textures/gui/sprites/container/slot.png");
+
     public BackpackSlot(Container container, int x, int y) {
         super(container, container.getContainerSize() - 1, x, y);
     }
@@ -13,5 +19,22 @@ public class BackpackSlot extends Slot {
     @Override
     public boolean mayPlace(ItemStack stack) {
         return stack.getItem() instanceof BackpackItem;
+    }
+
+    public void renderSurvival(GuiGraphics guiGraphics) {
+        renderSlot(guiGraphics, false);
+    }
+
+    public void renderCreative(GuiGraphics guiGraphics) {
+        renderSlot(guiGraphics, true);
+    }
+
+    private void renderSlot(GuiGraphics guiGraphics, boolean creative) {
+        if (creative) {
+            guiGraphics.blit(SLOT, this.x - 1, this.y - 1, 0, 0, 18, 18, 18, 18);
+        }
+        if (!hasItem()) {
+            guiGraphics.blit(BACKPACK_SPRITE, this.x - 1, this.y - 1, 0, 0, 18, 18, 18, 18);
+        }
     }
 }
--- common/src/main/java/net/xstopho/resource_backpacks/mixin/client/AbstractContainerScreenMixin.java
+++ common/src/main/java/net/xstopho/resource_backpacks/mixin/client/AbstractContainerScreenMixin.java
@@ -2,10 +2,7 @@ package net.xstopho.resource_backpacks.mixin.client;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.Slot;
-import net.xstopho.resource_backpacks.BackpackConstants;
 import net.xstopho.resource_backpacks.client.slot.BackpackSlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,18 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerScreen.class)
 public class AbstractContainerScreenMixin {
-
-    private static final ResourceLocation BACKPACK_SPRITE = BackpackConstants.of("textures/gui/sprites/container/backpack.png");
-    private static final ResourceLocation SLOT = ResourceLocation.withDefaultNamespace("textures/gui/sprites/container/slot.png");
-
     @Inject(at = @At("TAIL"), method = "renderSlot")
     public void renderSlot(GuiGraphics guiGraphics, Slot slot, CallbackInfo info) {
-        if (slot instanceof BackpackSlot) {
-            if ((Object) this instanceof InventoryScreen) {
-                if (!slot.hasItem()) {
-                    guiGraphics.blit(BACKPACK_SPRITE, slot.x - 1, slot.y - 1, 0, 0, 18, 18, 18, 18);
-                }
-            }
+        if (slot instanceof BackpackSlot backpackSlot) {
+            backpackSlot.renderSurvival(guiGraphics);
         }
     }
 }
