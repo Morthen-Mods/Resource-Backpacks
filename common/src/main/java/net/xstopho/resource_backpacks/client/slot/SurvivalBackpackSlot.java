42ed2e98a71f8d50d17e55146893852626bb34c1
Stein-N <nico-stein-98@outlook.de>
2025-01-14 19:44:07 +0100

    move BackpackSlot render helper methods back to AbstractContainerScreenMixin

--- common/src/main/java/net/xstopho/resource_backpacks/client/slot/SurvivalBackpackSlot.java
+++ common/src/main/java/net/xstopho/resource_backpacks/client/slot/SurvivalBackpackSlot.java
@@ -1,11 +1,8 @@
 package net.xstopho.resource_backpacks.client.slot;
 
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.xstopho.resource_backpacks.BackpackConstants;
 import net.xstopho.resource_backpacks.backpack.BackpackItem;
 
 public class BackpackSlot extends Slot {
@@ -19,22 +16,5 @@ public class BackpackSlot extends Slot {
     @Override
     public boolean mayPlace(ItemStack stack) {
         return stack.getItem() instanceof BackpackItem;
-    }
-
-    public void renderSurvival(GuiGraphics guiGraphics) {
-        renderSlot(guiGraphics, false);
-    }
-
-    public void renderCreative(GuiGraphics guiGraphics) {
-        renderSlot(guiGraphics, true);
-    }
-
-    private void renderSlot(GuiGraphics guiGraphics, boolean creative) {
-        if (creative) {
-            guiGraphics.blit(SLOT, this.x - 1, this.y - 1, 0, 0, 18, 18, 18, 18);
-        }
-        if (!hasItem()) {
-            guiGraphics.blit(BACKPACK_SPRITE, this.x - 1, this.y - 1, 0, 0, 18, 18, 18, 18);
-        }
     }
 }
