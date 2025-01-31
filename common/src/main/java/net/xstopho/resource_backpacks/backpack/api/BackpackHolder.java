00041084a9da2ea01114b9cb817393a986168f8b
Stein-N <nico-stein-98@outlook.de>
2025-01-31 17:50:09 +0100

    respect keepInventory rule

--- common/src/main/java/net/xstopho/resource_backpacks/backpack/api/BackpackHolder.java
+++ common/src/main/java/net/xstopho/resource_backpacks/backpack/api/BackpackHolder.java
@@ -3,8 +3,12 @@ package net.xstopho.resource_backpacks.backpack.api;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
+import net.xstopho.resource_backpacks.client.slot.BackpackSlot;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
@@ -13,13 +17,23 @@ public interface BackpackHolder {
     ItemStack resource_backpacks$getBackpack();
     void resource_backpacks$setBackpack(ItemStack backpack);
 
+    static void resource_backpacks$restoreBackpack(ServerPlayer oldPlayer, ServerPlayer newPlayer) {
+        ItemStack backpack = ((BackpackHolder) oldPlayer).resource_backpacks$getBackpack();
+        for (Slot slot : newPlayer.inventoryMenu.slots) {
+            if (slot instanceof BackpackSlot) {
+                slot.set(backpack);
+            }
+        }
+    }
+
     default void resource_backpacks$dropBackpack(LivingEntity entity) {
-        //TODO: add Gamerule Keep Inventory check
         ItemStack backpack = ((BackpackHolder) entity).resource_backpacks$getBackpack();
         if (!backpack.isEmpty()) {
             if (!entity.level().isClientSide()) {
-                entity.spawnAtLocation(backpack);
-                this.resource_backpacks$setBackpack(ItemStack.EMPTY);
+                if (!entity.getServer().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+                    entity.spawnAtLocation(backpack);
+                    this.resource_backpacks$setBackpack(ItemStack.EMPTY);
+                }
             }
         }
     }
--- fabric/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
+++ fabric/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
@@ -1,6 +1,7 @@
 package net.xstopho.resource_backpacks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -8,6 +9,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import net.xstopho.resource_backpacks.client.slot.BackpackHolder;
 import net.xstopho.resource_backpacks.network.payloads.EnderChestRequestPayload;
 import net.xstopho.resource_backpacks.network.payloads.EnderChestResponsePayload;
@@ -18,6 +20,12 @@ public class ResourceBackpacks implements ModInitializer {
     public void onInitialize() {
         BackpackConstants.commonInit();
         registerServerPayloads();
+
+        ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) -> {
+            if (oldPlayer.getServer().getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+                net.xstopho.resource_backpacks.backpack.api.BackpackHolder.resource_backpacks$restoreBackpack(oldPlayer, newPlayer);
+            }
+        });
 
         //TODO: remove with next update
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
