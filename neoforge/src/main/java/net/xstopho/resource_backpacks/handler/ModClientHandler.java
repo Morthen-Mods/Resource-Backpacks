2ec421442085523e390bcb9148d551c870cf5225
Stein-N <nico-stein-98@outlook.de>
2025-02-03 18:47:57 +0100

    remove old unused RenderLayer

--- neoforge/src/main/java/net/xstopho/resource_backpacks/handler/ModClientHandler.java
+++ neoforge/src/main/java/net/xstopho/resource_backpacks/handler/ModClientHandler.java
@@ -1,10 +1,5 @@
 package net.xstopho.resource_backpacks.handler;
 
-import net.minecraft.client.model.PlayerModel;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.state.PlayerRenderState;
-import net.minecraft.client.resources.PlayerSkin;
-import net.minecraft.world.entity.player.Player;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -13,7 +8,6 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.xstopho.resource_backpacks.BackpackConstants;
 import net.xstopho.resource_backpacks.client.BackpackModel;
-import net.xstopho.resource_backpacks.client.PlayerBackpackRenderLayer;
 import net.xstopho.resource_backpacks.registries.KeyMappingRegistry;
 
 @EventBusSubscriber(modid = BackpackConstants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -34,18 +28,5 @@ public class ModClientHandler {
     @SubscribeEvent
     public static void registerRenderLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(BackpackModel.BACKPACK_LAYER, BackpackModel::createLayer);
-    }
-
-    @SubscribeEvent
-    public static void addRenderLayer(EntityRenderersEvent.AddLayers event) {
-        LivingEntityRenderer<Player, PlayerRenderState, PlayerModel> playerRenderer = event.getSkin(PlayerSkin.Model.WIDE);
-        if (playerRenderer != null){
-            playerRenderer.addLayer(new PlayerBackpackRenderLayer(playerRenderer, event.getEntityModels()));
-        }
-
-        LivingEntityRenderer<Player, PlayerRenderState, PlayerModel> playerSlimRenderer = event.getSkin(PlayerSkin.Model.SLIM);
-        if (playerSlimRenderer != null) {
-            playerSlimRenderer.addLayer(new PlayerBackpackRenderLayer(playerSlimRenderer, event.getEntityModels()));
-        }
     }
 }
