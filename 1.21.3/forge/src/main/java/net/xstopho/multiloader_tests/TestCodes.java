4b9087d74d7379efb544a2d996405ac14db2aadf
Stein-N <nico-stein-98@outlook.de>
2025-02-04 19:46:33 +0100

    initial move to multiversion project

---
+++ 1.21.3/forge/src/main/java/net/xstopho/multiloader_tests/TestCodes.java
@@ -0,0 +1,12 @@
+package net.xstopho.multiloader_tests;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TestConstants.MOD_ID)
+public class TestCodes {
+
+    public TestCodes(FMLJavaModLoadingContext context) {
+
+    }
+}
---
+++ 1.21.4/neoforge/src/main/java/net/xstopho/multiloader_tests/TestCodes.java
@@ -0,0 +1,12 @@
+package net.xstopho.multiloader_tests;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(TestConstants.MOD_ID)
+public class TestCodes {
+
+    public TestCodes(IEventBus eventBus) {
+
+    }
+}
