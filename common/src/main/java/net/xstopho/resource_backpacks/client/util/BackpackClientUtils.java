5689f089f5229ca298a570fe07f7cece3d85b55e
Stein-N <nico-stein-98@outlook.de>
2025-02-04 06:55:50 +0100

    fix #39

--- common/src/main/java/net/xstopho/resource_backpacks/client/util/BackpackClientUtils.java
+++ common/src/main/java/net/xstopho/resource_backpacks/client/util/BackpackClientUtils.java
@@ -3,6 +3,9 @@ package net.xstopho.resource_backpacks.client.util;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
 public class BackpackClientUtils {
 
@@ -24,5 +27,17 @@ public class BackpackClientUtils {
 
         int keyCode = ((KeyMappingAccess) keyMapping).getKey().getValue();
         return InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), keyCode);
+    }
+
+    public static Entity getEntityById(int entityId) {
+        ClientLevel level = Minecraft.getInstance().level;
+        if (level == null) return null;
+
+        Entity entity = level.getEntity(entityId);
+
+        if (entity instanceof LivingEntity) {
+            return entity;
+        }
+        return null;
     }
 }
