792ac4f12ed63e3bc7c03b04480bbe62a69f226a
Stein-N <nico-stein-98@sbvl.de>
2025-03-28 13:05:08 +0100

    adding forge port for 1.21.5

---
+++ 1.21.5/forge/src/main/java/net/xstopho/resource_backpacks/ResourceBackpacks.java
@@ -0,0 +1,33 @@
+package net.xstopho.resource_backpacks;
+
+import net.minecraftforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.SimpleChannel;
+import net.xstopho.resource_backpacks.backpack.tooltip.CompactClientTooltipComponent;
+import net.xstopho.resource_backpacks.backpack.tooltip.InventoryClientTooltipComponent;
+import net.xstopho.resource_backpacks.network.BackpackNetworkRegistry;
+
+@Mod(BackpackConstants.MOD_ID)
+public class ResourceBackpacks {
+
+    public static SimpleChannel NETWORK;
+
+    public ResourceBackpacks(FMLJavaModLoadingContext context) {
+        context.getModEventBus().addListener(this::initCommon);
+        context.getModEventBus().addListener(this::registerTooltip);
+
+        BackpackConstants.commonInit();
+    }
+
+    private void initCommon(FMLCommonSetupEvent event) {
+
+        event.enqueueWork(BackpackNetworkRegistry::registerPayloads);
+    }
+
+    private void registerTooltip(RegisterClientTooltipComponentFactoriesEvent event) {
+        event.register(CompactClientTooltipComponent.CompactTooltipComponent.class, CompactClientTooltipComponent::new);
+        event.register(InventoryClientTooltipComponent.InventoryTooltipComponent.class, InventoryClientTooltipComponent::new);
+    }
+}
