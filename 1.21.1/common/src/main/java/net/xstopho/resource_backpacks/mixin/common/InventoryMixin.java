e1cc6963532ce9ba3e3b6b3c9d99601a6cb16062
Stein-N <nico-stein-98@sbvl.de>
2025-04-21 12:18:47 +0200

    first attempt for slowness penalty related to #49

---
+++ 1.21.1/common/src/main/java/net/xstopho/resource_backpacks/mixin/common/InventoryMixin.java
@@ -0,0 +1,60 @@
+package net.xstopho.resource_backpacks.mixin.common;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemContainerContents;
+import net.xstopho.resource_backpacks.backpack.BackpackItem;
+import net.xstopho.resource_backpacks.config.common.BackpackConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Inventory.class)
+public class InventoryMixin {
+
+    @Shadow NonNullList<ItemStack> items;
+    @Shadow Player player;
+
+    //TODO: - needs to be more robust
+    //      - rework code
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void resource_backpacks$tick(CallbackInfo ci) {
+//        if (BackpackConfig.slownessPenalty) {
+//            int count = 0;
+//
+//            for (ItemStack item : items) {
+//                if (item.getItem() instanceof BackpackItem backpack) {
+//                    ItemContainerContents container = item.get(DataComponents.CONTAINER);
+//                    if (container != null && !container.stream().toList().isEmpty()) {
+//                        count++;
+//                    }
+//                }
+//            }
+//
+//            if (player.hasEffect(MobEffects.MOVEMENT_SLOWDOWN)) return;
+//
+//            if (count > BackpackConfig.slownessOne) addPenalty(0);
+//            if (count > BackpackConfig.slownessTwo) addPenalty(1);
+//            if (count > BackpackConfig.slownessThree) addPenalty(2);
+//            if (count <= 2) removePenalty();
+//        }
+    }
+
+    @Unique
+    private void addPenalty(int amplifier) {
+        this.player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 10, amplifier));
+    }
+
+    @Unique
+    private void removePenalty() {
+        this.player.removeEffect(MobEffects.MOVEMENT_SLOWDOWN);
+    }
+}
